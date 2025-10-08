fbcc86fdedad28612108739144232b537e507250
danielhadgit <danielhadfield37@gmail.com>
2025-10-07 17:48:27 -0700

    Added Scripts

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+let c=a+b;
+// let is optional but please use, cleanest and newest way
+// variable is just like a placeholder
+// variable -> data type ex. string(alphanumeric ex. "dani23") or number
+// always use "" when storing data
+// boolean - values being true or false , empty being NULL
+let name = "Daniel";
+// quotes and single quotes are interchangeable unless you use a type of quote
+// in the value ex. "Daniel J'Hadfield"  or 'Daniel "James" Hadfield'
+
+let eligbility = false;  // BOOLEAN
+
+let bug = null; // Do not use reserved names - let, error, null
+
+let firstName = "Daniel";
+let lastName = "Hadfield";
+// When seperation is needed just capitalize the second word, CamelCasing
+
+// Console Log - display message for testing 
+console.log("my full name is "+firstName + lastName)
+console.log(typeof(eligbility));
+
+
+let x="2"; //string
+let y=1; //number
+let z=x+y; // 21 due to strings and numbers colliding
